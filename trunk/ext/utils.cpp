bd4b9524db2205b8d846499c049602384b2e27e1
isciurus@gmail.com <isciurus@gmail.com@5c9d5a02-fdfb-95ec-c6d5-8483a85edd53>
2011-11-23 17:07:19 +0000

    Added license, a modest code cleanup

--- trunk/ext/utils.cpp
+++ trunk/ext/utils.cpp
@@ -1,3 +1,31 @@
+/*
+* Windbg extension for VM traffic manipulation and analysis
+* 
+* Copyright 2011, isciurus. All rights reserved.
+* 
+* Redistribution and use in source and binary forms, with or without modification,
+* are permitted provided that the following conditions are met:
+* 
+* - Redistributions of source code must retain the above copyright notice, this
+*   list of conditions and the following disclaimer.
+*
+* - Redistributions in binary form must reproduce the above copyright notice, this
+*   list of conditions and the following disclaimer in the documentation and/or
+*   other materials provided with the distribution.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+* ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+* IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+* INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+* BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+* DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+* LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+* OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+* OF THE POSSIBILITY OF SUCH DAMAGE.
+*
+*/
+
 #include "dbgexts.h"
 
 void unescape(char *result, char *data)
@@ -10,42 +38,42 @@ void unescape(char *result, char *data)
 				break;
 
 			switch (ch) {
-			case 'a':				/* \a -> audible bell */
+			case 'a':
 			ch = '\a';
 			break;
 
-			case 'b':				/* \b -> backspace */
+			case 'b':
 			ch = '\b';
 			break;
 
-			case 'f':				/* \f -> formfeed */
+			case 'f':
 			ch = '\f';
 			break;
 
-			case 'n':				/* \n -> newline */
+			case 'n':
 			ch = '\n';
 			break;
 
-			case 'r':				/* \r -> carriagereturn */
+			case 'r':
 			ch = '\r';
 			break;
 
-			case 't':				/* \t -> horizontal tab */
+			case 't':
 			ch = '\t';
 			break;
 
-			case 'v':				/* \v -> vertical tab */
+			case 'v':
 			ch = '\v';
 			break;
 
-			case 'x':				/* \xnn -> ASCII value */
+			case 'x':
 			if(*(data + 1) != 0)
 				ch = strtol(data, NULL, 16);
 			data += 2;
 
 			break;
 
-			default:				/* \any -> any */
+			default:
 			break;
 			}
 		}
