0b5ef0d288eaa7a892b08000bfd7b21ec994760b
isciurus <isciurus@gmail.com>
2012-03-07 11:09:16 +0000

    Added some error handling code to the guest windbgshark installer script + recreate the service if already exists

--- install_guest.bat
+++ install_guest.bat
@@ -28,12 +28,37 @@ goto INSTALL
 :INSTALL
 
 copy %subfolder%\windbgshark_drv.sys %WINDIR%\system32\drivers /Y >nul
-sc create windbgshark_drv binpath= "system32\drivers\windbgshark_drv.sys" displayname= "windbgshark_drv" start= auto type= kernel
+
+IF NOT %ERRORLEVEL% == 0 (
+	echo Error copying driver module to %WINDIR%\system32\drivers
+	goto ERROR
+)
+
+sc create windbgshark_drv binpath= "system32\drivers\windbgshark_drv.sys" displayname= "windbgshark_drv" start= auto type= kernel >nul
+IF %ERRORLEVEL% == 1073 (
+	sc delete windbgshark_drv >nul
+	sc create windbgshark_drv binpath= "system32\drivers\windbgshark_drv.sys" displayname= "windbgshark_drv" start= auto type= kernel >nul
+)
+IF NOT %ERRORLEVEL% == 0 (
+	echo Error creating service for driver module
+	goto ERROR
+)
 
 reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\AeDebug" /v Debugger /t REG_SZ /d "\"%subfolder%\windbg.exe\" -p %%ld -e %%ld -g -QY -c \".load %subfolder%\wdbgshark_crashmon;!notifykdbg\"" /f >nul
 reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\AeDebug" /v Auto /t REG_SZ /d "1" /f >nul
 reg add "HKLM\SOFTWARE\Wow6432Node\Microsoft\Windows NT\CurrentVersion\AeDebug" /v Debugger /t REG_SZ /d "\"%subfolder%\windbg.exe\" -p %%ld -e %%ld -g -QY -c \".load %subfolder%\wdbgshark_crashmon;!notifykdbg\"" /f >nul
 reg add "HKLM\SOFTWARE\Wow6432Node\Microsoft\Windows NT\CurrentVersion\AeDebug" /v Auto /t REG_SZ /d "1" /f >nul
 
+IF NOT %ERRORLEVEL% == 0 (
+	echo Error installing crash monitor
+	goto ERROR
+)
+
 echo Windbgshark guest module installed successfully.
 pause
+exit
+
+:ERROR
+echo Windbgshark guest module not installed
+pause
+exit
